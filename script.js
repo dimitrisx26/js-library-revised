4459fdb9b614d5427e24cd492cb912647136e119
dimitrisx26 <dxynos@outlook.com>
2023-05-27 15:08:06 +0300

    all the functionalities are ready, remove and read button added

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const container = document.getElementById('container');
 document.getElementById("form").addEventListener("submit", function (event) {
     event.preventDefault();
-    addBookToLibrary();
 });
 
 let library = [];
@@ -19,8 +18,20 @@ library[2] = new Book("The Two Towers", "J.R.R. Tolkien", 352, "not read yet");
 library[3] = new Book("The Return of the King", "J.R.R. Tolkien", 416, "not read yet");
 library[4] = new Book("The Silmarillion", "J.R.R. Tolkien", 365, "not read yet");
 
-openForm = () => { document.getElementById('form').style.display = 'block'; }
-closeForm = () => { document.getElementById('form').style.display = 'none'; }
+openForm = () => { 
+    document.getElementById('form').style.display = 'block'; 
+    document.getElementById('popup-bg').classList.add('popup-bg');
+}
+
+closeForm = () => { 
+    document.getElementById('form').style.display = 'none'; 
+    document.getElementById('popup-bg').classList.remove('popup-bg');
+    addBookToLibrary();
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('pages').value = '';
+    document.getElementById('status').checked = false;
+}
 
 function addBookToLibrary() {
     let bookTitle = document.getElementById('title').value;
@@ -40,9 +51,9 @@ function addBookToLibrary() {
 function displayBooks() {
     container.innerHTML = '';
 
-    library.forEach(book => {
+    for (let i = 0; i < library.length; i++) {
         let bookTitle = document.createElement('h3');
-        bookTitle.textContent = book.title;
+        bookTitle.textContent = library[i].title;
 
         let infoContainer = document.createElement('div');
         infoContainer.classList.add('info-container');
@@ -54,19 +65,45 @@ function displayBooks() {
         const boldAuthor = document.createElement('strong');
         boldAuthor.textContent = 'Author: ';
         bookAuthor.appendChild(boldAuthor);
-        bookAuthor.append(book.author);
+        bookAuthor.append(library[i].author);
 
         let bookPages = document.createElement('span');
         const boldPages = document.createElement('strong');
         boldPages.textContent = 'Pages: ';
         bookPages.appendChild(boldPages);
-        bookPages.append(book.pages);
+        bookPages.append(library[i].pages);
 
         let bookStatus = document.createElement('span');
         const boldStatus = document.createElement('strong');
         boldStatus.textContent = 'Status: ';
         bookStatus.appendChild(boldStatus);
-        bookStatus.append(book.status);
+        bookStatus.append(library[i].status);
+
+        let editContainer = document.createElement('div');
+        editContainer.classList.add('edit-container');
+
+        let removeButton = document.createElement('div');
+        removeButton.classList.add('removeButton');
+        removeButton.addEventListener('click', function () {
+            library.splice(i, 1);
+            container.innerHTML = '';
+            displayBooks();
+        });
+
+        let readButton = document.createElement('div');
+        readButton.classList.add('readButton');
+        readButton.addEventListener('click', function () {
+            if (library[i].status === "not read yet") {
+                library[i].status = "read";
+                container.innerHTML = '';
+                displayBooks();
+            }
+            else {
+                library[i].status = "not read yet";
+                container.innerHTML = '';
+                displayBooks();
+            }
+        });
 
         container.appendChild(infoContainer);
         infoContainer.appendChild(bookTitle);
@@ -74,7 +111,10 @@ function displayBooks() {
         bookInfo.appendChild(bookAuthor);
         bookInfo.appendChild(bookPages);
         bookInfo.appendChild(bookStatus);
-    });
+        infoContainer.appendChild(editContainer);
+        editContainer.appendChild(readButton);
+        editContainer.appendChild(removeButton);
+    }
 }
 
 displayBooks();
